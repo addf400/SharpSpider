748c056cd1c025e4ffc15945deb8251375071eca
Hangbo Bao <addf400@foxmail.com>
2017-10-15 23:45:58 +0800

    change gitignore file and add avoid repeat

--- NetEaseSpider/Program.cs
+++ NetEaseSpider/Program.cs
@@ -11,13 +11,24 @@ namespace NetEaseSpider
         {
             var spider = new Spider();
 
-            var ids = File.ReadAllLines("songerlist.txt", Encoding.UTF8).Select(s => int.Parse(s.Split('\t').First().Trim().Split('=').Last())).ToArray();
+            var ids = File.ReadAllLines("..\\TmpData\\songerlist.txt", Encoding.UTF8).Select(s => int.Parse(s.Split('\t').First().Trim().Split('=').Last())).ToHashSet();
+            var doneIds = File.ReadAllLines("..\\TmpData\\info.txt", Encoding.UTF8);
+            foreach (var line in doneIds)
+            {
+                var parts = line.Split('\t');
+                if (!parts.Last().Equals("Done"))
+                    Console.WriteLine("Error !");
+                int id = int.Parse(parts.First());
+                if (ids.Contains(id))
+                    ids.Remove(id);
+            }
 
-            Console.WriteLine("Loading {0} songerid", ids.Length);
+
+            Console.WriteLine("Loading {0} songerid", ids.Count);
             spider.SetLogger(
-                "log.txt", 
-                "info.txt");
-            spider.SongerId2AlbumsId(ids);
+                "..\\TmpData\\log.txt",
+                "..\\TmpData\\info.txt");
+            spider.SongerId2AlbumsId(ids.ToArray());
 
             spider.Close();
 
