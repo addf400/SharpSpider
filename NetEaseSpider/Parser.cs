490ee22b0f50165a39c197b80e311956a7bca59e
addf-msi <addf400@foxmail.com>
2017-11-17 19:11:56 +0800

    just a commit

---
+++ NetEaseSpider/Parser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetEaseSpider
+{
+    class Parser
+    {
+        public static void GetAlbumsId(string source)
+        {
+
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
 
 namespace Test
 {
@@ -6,8 +9,25 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            int albumId = 18966;
 
-            Console.WriteLine("Hello World!");
+            string url = String.Format("http://music.163.com/api/album/{0}?ext=true&id={0}&offset=0&total=true&limit=10", albumId);
+
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36";
+            request.Referer = "http://music.163.com/";
+            CookieContainer cookie = new CookieContainer();
+            Uri uri = new Uri("http://music.163.com/");
+            cookie.SetCookies(uri, "appver=1.5.0.75771;");
+            request.CookieContainer = cookie;
+            Console.WriteLine(request.CookieContainer.GetCookies(uri));
+
+            var response = request.GetResponse() as HttpWebResponse;
+            Stream responseStream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(responseStream, Encoding.UTF8);
+            var html = sr.ReadToEnd();
+
+            Console.WriteLine(html);
         }
     }
 }
